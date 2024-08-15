553bebb21f37ded83821a86ab6193ab9b77f82c6
Aaron Kofi Haizel <kofi12@my.yorku.ca>
2024-08-15 16:25:27 -0400

    made a few table changes

--- models/models.py
+++ models/models.py
@@ -5,8 +5,8 @@ class Tenant (SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     first_name: str
     last_name: str
-    admission_date: datetime = Field()
-    discharge_date: datetime | None
+    admission_date: datetime | None = Field(default = None)
+    discharge_date: datetime | None = Field(default=None)
 
 class User (SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
